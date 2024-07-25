ab1c1a4e4a1197314613a4ef5e1d27304821dda4
raskal-dev <kalvinrasamyzo@gmail.com>
2024-07-25 09:39:25 +0300

    variavles

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
 alert('Hello World !');
+
+var name = "Kalvin"; //Les variables déclarées avec var peuvent être modifiées
+
+let age = 25; //Les variables déclarées avec let peuvent être modifiées
+
+const TVA = 20;
+
+const PI = 3.14; //Les variables déclarées avec const ne peuvent pas être modifiées
+
